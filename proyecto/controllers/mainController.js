fd33938bd9ed07f597c8bd75cb4ca23ac8486c96
fsinDH <fsin@udesa.edu.ar>
2022-04-14 18:32:07 -0300

    Cambiando headers en views

--- proyecto/controllers/mainController.js
+++ proyecto/controllers/mainController.js
@@ -2,7 +2,6 @@ let products = require("../db/products");
 const mainController = {
     index: function (req, res) {
       return res.render('index', {comics: products.lista});
-      //return res.render("hola");
     },
     
   };
