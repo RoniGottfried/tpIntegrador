9ab26b81faa813323f1e436984948f57b791887f
RoniGottfried <rgottfried@edu.ar>
2022-06-18 12:15:57 -0300

    cambios en el config.js y cambios en el login

--- proyecto/database/config/config.js
+++ proyecto/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
-    "database": "comicsFRL",
+    "password": null,
+    "database": "comicsfrl",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
