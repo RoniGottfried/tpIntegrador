f7631c9fb221c35247070013b99e4b4bfceaa95c
RoniGottfried <rgottfried@edu.ar>
2022-06-21 10:54:47 -0300

    el app.use de express-session

--- proyecto/app.js
+++ proyecto/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var mainRouter = require('./routes/main');
 var usersRouter = require('./routes/users');
@@ -22,6 +23,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  // el objeto literal tiene tres propiedades que siempre son las mismas
+  secret: 'ComicsDB',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use('/', mainRouter);
 app.use('/users', usersRouter);
